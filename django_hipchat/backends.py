c252b7b609f824fe1340950b70ba9a42ee9c0dbb
Chris Lamb <chris@thread.com>
2014-04-02 21:09:19 +0100

    Add a console backend for debugging.
    Signed-off-by: Chris Lamb <chris@thread.com>

--- django_hipchat/backends.py
+++ django_hipchat/backends.py
@@ -1,3 +1,4 @@
+import pprint
 import urllib2
 
 def urllib(url, data, fail_silently):
@@ -8,3 +9,8 @@ def urllib(url, data, fail_silently):
     except Exception:
         if not fail_silently:
             raise
+
+def console(url, data, fail_silently):
+    print "-" * 80
+    print "django-hipchat:"
+    pprint.pprint(data, indent=4)
