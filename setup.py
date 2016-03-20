62bcad9c81e0ba58ef2426f274c648c4a5b1a6f9
Chris Lamb <chris@chris-lamb.co.uk>
2016-03-20 20:55:30 +0800

    Releasing version 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-hipchat',
 
-    version='1.0.1',
+    version='1.0.2',
     description="Provides easy-to-use integration between Django projects and "
         "the hipchat group chat and IM tool.",
 
